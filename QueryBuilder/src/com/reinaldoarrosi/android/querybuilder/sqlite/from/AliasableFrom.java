b948abb06f45fbab97c76bd93c7346516c9d34a9
reinaldoarrosi <rarrosi@gmail.com>
2014-04-24 10:32:30 -0300

    plain JAVA compatibility
    changed project to be compatible to plain JAVA, not only Android. Now we
    can generate a .jar file and use it everywhere

--- QueryBuilder/src/com/reinaldoarrosi/android/querybuilder/sqlite/from/AliasableFrom.java
+++ QueryBuilder/src/com/reinaldoarrosi/android/querybuilder/sqlite/from/AliasableFrom.java
@@ -3,6 +3,7 @@ package com.reinaldoarrosi.android.querybuilder.sqlite.from;
 public abstract class AliasableFrom<T> extends From {
 	protected String alias;
 	
+	@SuppressWarnings("unchecked")
 	public T as(String alias) {
 		this.alias = alias;
 		return (T)this;
